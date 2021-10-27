4c1de45d37c488389c9808e3c4e56843de011d79
Stillwell, Charles Calvin <cstillwell@usgs.gov>
2021-10-27 17:47:20 -0400

    Update src files to call functions not main script

--- _targets.R
+++ _targets.R
@@ -1,6 +1,9 @@
 library(targets)
-source("pipelines_targets_1.R")
-tar_option_set(packages = c("tidyverse", "sbtools", "whisker"))
+source("1_fetch/src/fetch_data.R")
+source("2_prep/src/prep_data.R")
+source("3_plot/src/plot_data.R")
+source("4_summary/src/summarise_data.R")
+tar_option_set(packages = c("tidyverse", "sbtools"))
 
 list(
   # Get the data from ScienceBase
@@ -8,7 +11,7 @@ list(
     new_data_raw,
     fetch_data(out_filepath = "1_fetch/out/new_mendota_data.csv"),
     format = "file"
-  ), 
+  ),
   # Prepare the data for plotting
   tar_target(
     new_data_plot,
@@ -17,23 +20,24 @@ list(
   # Create a plot
   tar_target(
     new_figure_1,
-    plot_data(out_filepath = "3_plot/out/new_figure_1.png", 
-              data = new_data_plot), 
+    plot_data(out_filepath = "3_plot/out/new_figure_1.png",
+              data = new_data_plot),
     format = "file"
   ),
   # Save the processed data
   tar_target(
     new_model_results,
-    write_csv(new_data_plot, 
-              file = "3_plot/out/new_data_plot.csv"), 
+    write_csv(new_data_plot,
+              file = "3_plot/out/new_data_plot.csv"),
     format = "file"
   ),
   # Save the model diagnostics
   tar_target(
     new_model_summary,
-    summarise_data(out_filepath = "4_summary/out/new_summary.txt", 
-                   data = new_data_plot), 
+    summarise_data(out_filepath = "4_summary/out/new_summary.txt",
+                   data = new_data_plot),
     format = "file"
   )
 )
 
+tar_make()
